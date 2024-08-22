7d1109a9427ee22341c2589a8929ad1a82712cf6
“anabeatty” <anabmdeazevedo@gmail.com>
2024-08-21 19:41:54 -0700

    Lab6youmademesad

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+function qualquercoisa() {
+  let contact_name = document.getElementById("contact_name").textContent;
+  let button = document.getElementById("button");
+
+  if (contact_name == "") {
+    alert("Name is a mandatory field");
+  } else {
+    if (contact_name.length < 5) {
+      alert("Name: Minimum 5 characters.");
+    }
+  }
+
+  if (contact_email == "") {
+    alert("Email is a mandatory field");
+  }
+
+  if ((contact_phone = "")) {
+    alert("Phone is a mandatory field");
+  }
+
+  if ((contact_message = "")) {
+    alert("Message is a mandatory field");
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,2 @@
+let preferredlanguage = prompt("Enter your preferred programming language:");
+document.getElementById("proglang").textContent = preferredlanguage;
